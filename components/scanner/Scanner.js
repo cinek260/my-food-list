ec14101ec9fc1c18a135e729f7a5b7eeb3d3bcb5
Marcin Kadłubowski <cinek260@gmail.com>
2017-08-10 19:54:23 +0200

    add scanner

---
+++ components/scanner/Scanner.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View
+} from 'react-native';
+import Camera from 'react-native-camera';
+
+export default class Scanner extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Camera
+          ref={cam => this.camera = cam}
+          onBarCodeRead={this.handleBarCodeRead.bind(this)}
+          style={styles.preview}>
+          <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+        </Camera>
+      </View>
+    );
+  }
+
+  handleBarCodeRead({type, data, bounds}) {
+    console.log('type', type);
+    console.log('data', data);
+    console.log('bounds', bounds);
+  }
+
+  takePicture() {
+    const options = {};
+    //options.location = ...
+    this.camera.capture({metadata: options})
+      .then((data) => console.log(data))
+      .catch(err => console.error(err));
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  }
+});
