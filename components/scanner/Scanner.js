1e7f86c00060618d550ded58ddcb68ad5aec5ae3
Marcin Kadłubowski <cinek260@gmail.com>
2017-08-11 13:36:14 +0200

    prototype works

--- components/scanner/Scanner.js
+++ components/scanner/Scanner.js
@@ -1,38 +1,117 @@
 import React, { Component } from 'react';
 import {
-  // AppRegistry,
-  // Dimensions,
   StyleSheet,
   Text,
-  // TouchableHighlight,
-  View
+  View,
+  Button
 } from 'react-native';
 import Camera from 'react-native-camera';
+import Base64 from 'base-64';
+import env from '../../env';
 
 export default class Scanner extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      type: null,
+      data: null,
+      bounds: null,
+      codeReadedTime: null
+    }
+    this.basicAuth = Base64.encode(`${env.gs1ApiUserName}:${env.gs1ApiPassword}`);
+  }
+
   render() {
+    const { bounds } = this.state;
     return (
       <View style={styles.container}>
         <Camera
           ref={cam => this.camera = cam}
-          onBarCodeRead={this.handleBarCodeRead.bind(this)}
+          onBarCodeRead={this.handleBarCodeRead}
           style={styles.preview}>
-          <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+          <View style={this.setScannerStyle()}></View>
+          <View style={this.setButtonStyle()}>
+            <Text style={this.setTextButtonStyle()} onPress={this.scanCode}>Scan</Text>
+          </View>
         </Camera>
       </View>
     );
   }
 
-  handleBarCodeRead({type, data, bounds}) {
-    console.log('data', data);
+  handleBarCodeRead = ({type, data, bounds}) => {
+    this.setState({ type, data, bounds, codeReadedTime: new Date().getTime() });
+    setTimeout(() => this.hideBarcodeScanner(), 300)
   }
 
-  takePicture() {
-    const options = {};
-    //options.location = ...
-    this.camera.capture({metadata: options})
-      .then((data) => console.log(data))
-      .catch(err => console.error(err));
+  hideBarcodeScanner = () => {
+    const { codeReadedTime } = this.state;
+    if (codeReadedTime + 300 <= new Date().getTime()) {
+      this.setState({ type: null, data: null, bounds: null, codeReadedTime: null });
+    }
+  }
+
+  scanCode = async () => {
+    const { type, data } = this.state;
+    try {
+      let response = await fetch(`https://www.produktywsieci.gs1.pl/api/products/${data}?aggregation=SOCIAL`, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Basic ${this.basicAuth}`,
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        }
+      })
+      let responseJson = await response.json();
+      console.log(responseJson);
+      return;
+    } catch(error) {
+      console.error(error);
+    }
+
+  }
+
+  setScannerStyle = () => {
+    const { bounds } = this.state;
+    const styles = bounds ? {
+      width: Math.round(bounds.size.width),
+      height: Math.round(bounds.size.height),
+      top: Math.round(bounds.origin.y),
+      left: Math.round(bounds.origin.x),
+      position: 'absolute',
+      borderWidth: 2,
+      borderColor: 'red'
+    } : {
+      display: 'none'
+    }
+    return styles;
+  }
+
+  setButtonStyle = () => {
+    const { data } = this.state;
+    const styles = {
+      padding: 10,
+      margin: 40,
+      borderWidth: 5,
+      borderColor: data ? 'rgba(255, 0, 0, 1.0)' : 'rgba(255, 0, 0, .5)',
+      backgroundColor: data ? 'rgba(255, 255, 255, 1.0)' : 'rgba(255, 255, 255, .5)',
+      borderRadius: 50,
+      width: 100,
+      height: 100,
+      justifyContent:'center'
+    }
+    return styles;
+  }
+
+  setTextButtonStyle = () => {
+    const { data } = this.state;
+    const styles = {
+      fontSize: 20,
+      textAlign: 'center',
+      fontWeight: 'bold',
+      color: data ? 'rgba(0, 0, 0, 1.0)' : 'rgba(0, 0, 0, .5)'
+    }
+    return styles;
   }
 }
 
@@ -45,13 +124,5 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'flex-end',
     alignItems: 'center'
-  },
-  capture: {
-    flex: 0,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    color: '#000',
-    padding: 10,
-    margin: 40
   }
 });
